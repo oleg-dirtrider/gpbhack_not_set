5cc066c160664dd1b273865806ea1320c878c484
Vasily Mikhaylov <vasyanch852@gmail.com>
2021-05-29 22:02:52 +0300

    beta 0.3 db.py

--- code/db.py
+++ code/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import registry, relationship, Session
 
 
@@ -34,10 +34,12 @@ class CampaignBlock(Base):
     block_name = Column(String)
     block_type = Column(String)
     subdiagram_id = Column(String)
+    subdiagram_name = Column(String)
     campaign_id = Column(Integer, ForeignKey(f'{SCHEMA}.campaigns.id'), nullable=False)
     campaign = relationship("Campaign", back_populates="campaigns_blocks")
-    data_process_id = Column(Integer, ForeignKey(f"{SCHEMA}.data_processes.id"), nullable=False)
-    data_process = relationship("DataProcess", back_populates="campaigns_blocks")
+    data_processes = relationship(
+        "DataProcess", secondary="campaigns_blocks_data_processes_association_table", back_populates='campaigns_blocks'
+    )
 
     def __repr__(self):
         return f"Campaign(id={self.id!r}, sas_block_id={self.sas_block_id!r}, block_name={self.block_name!r})"
@@ -51,27 +53,32 @@ class DataProcess(Base):
     data_process_name = Column(String)
     lib_name = Column(String)
     table_name = Column(String)
-    campaigns_blocks = relationship("CampaignBlock", back_populates="data_process")
+    campaigns_blocks = relationship(
+        "CampaignBlock", secondary="campaigns_blocks_data_processes_association_table", back_populates="data_processes"
+    )
 
     def __repr__(self):
         return f"Campaign(id={self.id!r}, sas_data_process_id={self.sas_data_process_id!r}, " \
                f"data_process_name={self.data_process_name!r})"
 
 
+campaigns_blocks_data_processes_association_table = Table(
+    'campaigns_blocks_data_processes_association', mapper_registry.metadata,
+    Column('campaign_block', Integer, ForeignKey(f'{SCHEMA}.campaigns_blocks.id'), nullable=False),
+    Column('data_process', Integer, ForeignKey(f'{SCHEMA}.data_processes.id'), nullable=False),
+    schema=SCHEMA
+)
+
+
 class DBRunner:
-    def __init__(self, connection_string: str = "sqlite+pysqlite:///gpbhack.db") -> None:
+    def __init__(self, connection_string: str = db_url) -> None:
         self.connection_string = connection_string
         self.engine = create_engine(self.connection_string, echo=True, future=True)
 
     def insert_data(self, data: dict):
         session = Session(self.engine)
-        # table_map = {
-        #     "blocks": Campaign,
-        #     "campaigns": CampaignBlock,
-        #     "data_process": DataProcess
-        # }
         campaign_dict = {}
-        data_process_dict = {}
+        block_dict = {}
         for row in data["campaigns"]:
             row_instance = Campaign(
                 sas_campaign_id=row.id,
@@ -80,6 +87,18 @@ class DBRunner:
             session.add(row_instance)
             session.flush()
             campaign_dict[row_instance.sas_campaign_id] = row_instance.id
+        for row in data["blocks"]:
+            row_instance = CampaignBlock(
+                sas_block_id=row.id,
+                block_name=row.name,
+                block_type=row.type,
+                subdiagram_id=row.subdiagram_id,
+                subdiagram_name=row.subdiagram_name,
+                campaign_id=campaign_dict[row.campaign_id],
+            )
+            session.add(row_instance)
+            session.flush()
+            block_dict[row_instance] = row.data_process_id_list
         for row in data["data_processes"]:
             row_instance = DataProcess(
                 sas_data_process_id=row.id,
@@ -89,46 +108,11 @@ class DBRunner:
             )
             session.add(row_instance)
             session.flush()
-            data_process_dict[row_instance.sas_data_process_id] = row_instance.id
-        for row in data["blocks"]:
-            row_instance = CampaignBlock(
-                sas_block_id=row.id,
-                block_name=row.name,
-                block_type=row.type,
-                subdiagram_id=row.subdiagram_id,
-                campaign_id=campaign_dict[row.campaign_id],
-                data_process_id=data_process_dict.get(row.data_process_id, 1)
-            )
+            for campaign_block_instance, data_processes_id_list in block_dict.items():
+                data_process_campaign_block = []
+                if row_instance.sas_data_process_id in data_processes_id_list:
+                    data_process_campaign_block.append(campaign_block_instance)
             session.add(row_instance)
             session.flush()
-
-
-
-        # c = Campaign(
-        #     sas_campaign_id='test',
-        #     campaign_name = "test",
-        # )
-        # d = DataProcess(
-        #     sas_data_process_id = "test",
-        #     data_process_name = "test",
-        #     lib_name = "test",
-        #     table_name = "test",
-        # )
-        # b = CampaignBlock(
-        #     sas_block_id="test",
-        #     block_name = "test",
-        #     block_type = "test",
-        #     subdiagram_id = "test",
-        #     campaign_id = 1,
-        #     data_process_id = 1
-        # )
-        # session.add(c)
-        # session.add(d)
-        # session.flush()
-        # session.add(b)
         session.commit()
         session.close()
-
-
-
-
